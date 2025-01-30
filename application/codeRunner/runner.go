0bd8dbf0de09f25a7a2aef824a187579b8f8124c
karkulevskiy <karkulevskiy@yandex.ru>
2025-01-30 08:54:47 +0300

    add new template for running code in docker

--- application/codeRunner/runner.go
+++ application/codeRunner/runner.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -22,26 +24,35 @@ func New() *CodeRunner {
 
 func (r *CodeRunner) Run(code string) (string, error) {
 	// TODO нужно быстро уметь валидировать, что код вообще билдиться
-	file, err := os.CreateTemp("", "code-*.cpp")
+	// TODO сделать унифицированную папку
+	file, err := os.CreateTemp("/home/spacikl/codes", "code-*.py")
 	if err != nil {
 		return "", err
 	}
 	defer os.Remove(file.Name())
 
-	// TODO - идея постоянной перезаписи файлов, чтобы не создавать каждый файл. Или лучше - прокинуть эти файлы сразу в докер
-	// и скриптами туда прокидывать код. Надо подумать ...
-
 	if _, err := file.WriteString(code); err != nil {
 		return "", err
 	}
 
-	containerName := "code-runner" // TODO просто как пример
-	cmd := exec.Command("docker", "run", "--rm", "-v",
-		fmt.Sprintf("%s:/code.cpp", file.Name()), // TODO научиться собирать мета инфу о файле, чтобы понимать в каком окружении собирать
-		containerName, "sh", "-c", "g++ /code.cpp -o /code && /code")
+	if err := file.Close(); err != nil {
+		return "", err
+	}
 
-	// Запускаем Docker
+	fileName, ok := strings.CutPrefix(file.Name(), "/home/spacikl/codes/")
+	if !ok {
+		return "", fmt.Errorf("write here error")
+	}
+	fileName = "/home/" + fileName
+	slog.Info(fileName)
+	containerName := "code-runner"
+	//  Параметры запуска: docker run --name code-runner -v /home/spacikl/codes/:/home/ code-runner
+	cmd := exec.Command("docker", "exec", containerName, "python3", fileName)
+
+	slog.Info(cmd.String())
+
 	var stdout, stderr bytes.Buffer
+
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
@@ -52,5 +63,5 @@ func (r *CodeRunner) Run(code string) (string, error) {
 		return "", err
 	}
 
-	return "hello", nil
+	return stdout.String(), nil
 }
